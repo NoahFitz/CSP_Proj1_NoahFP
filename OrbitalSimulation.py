03640fd7a8cde6a67e62c83e9c47a1fb61957492
Noah FitzPatrick <nfitzpatrick1@cub.uca.edu>
2021-03-23 11:49:46 -0500

    Added Function to Read in a and e for two planets used

--- OrbitalSimulation.py
+++ OrbitalSimulation.py
@@ -3,9 +3,13 @@
 #Takes in orbital data of two planets and calculates the distance between the two
 import numpy as np
 
-def ReadOrbitalData():
+def ReadOrbitalData(): #Takes in data for the two planets
     orbit_data = np.loadtxt('OrbitData.csv', delimiter =",", skiprows = 1)
-    print(orbit_data)
+    return orbit_data
+print(ReadOrbitalData())
 
-ReadOrbitalData()
-
+def GenerateOrbitalPlot():
+    e1 = ReadOrbitalData()[0][0]
+    e2 = ReadOrbitalData()[1][0]
+    a1 = ReadOrbitalData()[0][1]
+    a2 = ReadOrbitalData()[1][1]
