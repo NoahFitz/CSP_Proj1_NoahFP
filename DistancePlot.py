899eacbd851871cd0a3378924dd5bfb39154e511
Noah FitzPatrick <nfitzpatrick1@cub.uca.edu>
2021-03-31 20:12:43 -0500

    Tried decreasing the velocity interval, it did not go so well. My computer sat in awkward silence for a good long time before I called it quits

--- DistancePlot.py
+++ DistancePlot.py
@@ -3,12 +3,16 @@ import matplotlib.pyplot as plt
 from pylab import savefig, plot
 import numpy as np
 def DistPlot():
+    fig = plt.figure()
+    fig.suptitle('Earth-Mars Distance over 20 days (Starting 3/31/2021)')
     t = np.linspace(0,10,10)
     D = np.array([])
-    for i in range(10):#range(900)
+    for i in range(10):
         T = i*86400
         D = np.append(D, [Difference(T)*10**-10])
     plot(t, D, "k.")
+    plt.xlabel("Time in Days")
+    plt.ylabel("Disance in 10^10 Meters")
     savefig('DistancePlot')
 DistPlot()
 
